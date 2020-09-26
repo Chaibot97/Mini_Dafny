094523a39d29980af5b30af8d3eb130b28f52565
Junrui Liu <junrui.liu@yale.edu>
2020-09-26 08:00:35 -0400

    Definition for guarded commands

--- vcgen/src/VCGen.hs
+++ vcgen/src/VCGen.hs
@@ -5,6 +5,13 @@ import Parser.Parser
 
 import System.Environment
 
+data GuardedCommand = GCAssert Assertion
+                    | GCAssume Assertion
+                    | GCHavoc  Name
+                    | GCChoice GCBlock GCBlock
+
+type GCBlock = [GuardedCommand]
+
 main :: IO ()
 main = do
     as <- getArgs
